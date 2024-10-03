90f374b785cdf40fcd2e89204596bef718b7cdc5
Thomas Phan <phanthomas.tai@gfk.com>
2024-10-03 23:45:53 +0700

    amed

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ document.getElementById('reset').addEventListener('click', function () {
   ipc.send('reset');
 });
 document.getElementById('start').addEventListener('click', function () {
-  let dataText = document.getElementById('datatext').value || "dakmak847789352@hotmail.com|Limong47@\nbloodaroundfanta2777844@hotmail.com|Limong47@\ntheendstory7910616@hotmail.com|Limong47@";
-  let proxyText = document.getElementById('proxytext').value || "f1447450d533ffc3891fdfa78be9a343";
+  let dataText = document.getElementById('datatext').value;
+  let proxyText = document.getElementById('proxytext').value;
   ipc.send('start', dataText, proxyText);
 });
 
